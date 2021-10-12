9f7c6d26367f27c4bb109452870ba3bc32b2d615
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-10-12 21:57:06 +0530

    Created a Tkinter Window

--- YT Video Downloader.py
+++ YT Video Downloader.py
@@ -3,3 +3,23 @@ from pyyoutube import Api
 from pytube import YouTube
 from threading import Thread
 from tkinter import messagebox
+
+# Create Object
+root = Tk()
+# Set geometry
+root.geometry('400x200')
+
+# Add Label
+Label(root, text="Yt Playlist Downloader",
+	font="italic 15 bold").pack(pady=10)
+Label(root, text="Enter the Playlist URL:-", font="italic 10").pack()
+
+# Add Entry box
+playlistId = Entry(root, width=60)
+playlistId.pack(pady=5)
+
+download_start = Button(root, text="Start The Download", command=threading)
+download_start.pack(pady=10)
+
+# Execute Tkinter
+root.mainloop()
